ab489cb9289704ac308dfddd28bebb49938daaa9
iceie2 <78112083+iceie2@users.noreply.github.com>
2025-12-18 07:08:27 -0500

    0.001

--- snake.py
+++ snake.py
@@ -10,3 +10,24 @@ wn = turtle.Screen()
 wn.title ("snake")
 wn.bgcolor("Gray")
 wn.setup(width=600, height=600)
+
+#head
+head = turtle.Turtle()
+head.shape("circle")
+head.color("black")
+head.penup()
+head.goto(0,0)
+
+#body
+
+#food
+food = turtle.Turtle()
+
+#Move the snake
+
+#collision
+
+
+
+
+wn.mainloop()
